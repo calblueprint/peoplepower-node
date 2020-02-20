dbcd31dff025fa5b37a428e3e8a90322aac2a89a
aivantg <aivant.developer@gmail.com>
2020-02-20 13:49:35 -0800

    added basic index file

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const fetch = require('node-fetch')
-const env = require('dotenv-safe').config();
+require('dotenv-safe').config(); // Set up environment variables
+const fetch = require('node-fetch'); // Import fetch (for network requests)
+
+// This function (for now) will be what is called when the program is run.
+// Use this to help test other functions
+function main() {
+  console.log('Currently there is nothing here!');
+}
+
+// Add other functions below that perform the individual operations
+// Example: (Pull data from EnPhase API, Generate PDF given billing data, etc.)
+
+main();
