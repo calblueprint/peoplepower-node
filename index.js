3f4e6884e0fb43cdb39190bfb28c8a81a37d6fb6
aivantg <aivant.developer@gmail.com>
2020-02-23 16:48:40 -0800

    working enphase function

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv-safe').config(); // Set up environment variables
 const fetch = require('node-fetch'); // Import fetch (for network requests)
+const moment = require('moment');
 
 const EnphaseUtils = require('./utils/enphase.js');
 
 async function main() {
   console.log('Testing Enphase Data');
   const result = await EnphaseUtils.getEnphaseData(
-    process.env.ENPHASE_KEY,
     process.env.SAMPLE_ENPHASE_USER_ID,
-    process.env.SAMPLE_ENPHASE_SYSTEM_ID
+    process.env.SAMPLE_ENPHASE_SYSTEM_ID,
+    moment().subtract(30, 'days'),
+    moment()
   );
   console.log(result);
 }
--- utils/enphase.js
+++ utils/enphase.js
@@ -1,56 +1,24 @@
 const fetch = require('node-fetch'); // Import fetch (for network requests)
+const moment = require('moment');
 
-// API call to get enery over lifetime of system
-const getLifetimeEnergyData = async (key, userId, systemId) => {
+// SEE ENPHASE API DOCS HERE https://developer.enphase.com/docs
+const getEnphaseData = async (userId, systemId, startDate, endDate) => {
   // Build API Url
   const baseUrl = 'https://api.enphaseenergy.com/api/v2';
+  const key = process.env.ENPHASE_KEY;
 
-  const url = `${baseUrl}/systems/${systemId}/energy_lifetime?key=${key}&user_id=${userId}&production=all`;
-
-  const options = {
-    muteHttpExceptions: true,
-    headers: {
-      Accept: 'application/json'
-    }
+  const queryData = {
+    key,
+    user_id: userId,
+    production: 'all',
+    start_date: moment(startDate).format('YYYY-MM-DD'),
+    end_date: moment(endDate).format('YYYY-MM-DD')
   };
 
-  const response = await fetch(url, options);
-  return response.json();
-};
-
-// date array
-function getDateArray(start, length) {
-  var arr = new Array(),
-    dt = new Date(start);
-
-  while (arr.length < length) {
-    arr.push(new Date(dt.getFullYear(), dt.getMonth(), dt.getDate()));
-    dt.setDate(dt.getDate() + 1);
-  }
-
-  return arr;
-}
-
-const getEnphaseData = async (key, userId, systemId) => {
-  // SEE ENPHASE API DOCS HERE https://developer.enphase.com/docs
-
-  // parse response data from energy lifetime
-  const lifetimeData = await getLifetimeEnergyData(key, userId, systemId);
-
-  return lifetimeData;
-  var start_date = new Date(data.start_date);
-  var meter_production = data.meter_production;
-  var micro_production = data.micro_production;
-  var date_array = getDateArray(start_date, meter_production.length);
-
-  return [date_array, micro_production, meter_production];
-  //   // WRITE TO SHEET
-  //   // set active sheet and range
-  //   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  //   var sheet = spreadsheet.getSheets()[2];
-  //   var range = sheet.getRange(1, 2, 3, meter_production.length);
-  //   range.setValues([date_array, micro_production, meter_production]);
-  //   Logger.log(range.getValues());
+  const queryString = new URLSearchParams(queryData).toString();
+  const url = `${baseUrl}/systems/${systemId}/energy_lifetime?${queryString}`;
+  const response = await fetch(url);
+  return await response.json();
 };
 
 module.exports = {
