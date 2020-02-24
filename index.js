c955fded53e872f9acde5731bc6f5c6ead8abdf3
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2020-02-23 19:31:27 -0800

    update 4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv-safe').config(); // Set up environment variables
+feoaijfeoia`require('dotenv-safe').config(); // Set up environment variables
 const fetch = require('node-fetch'); // Import fetch (for network requests)
-
+ feafeafa
 // This function (for now) will be what is called when the program is run.
 // Use this to help test other functions
 function main() {
---
+++ test.js
@@ -0,0 +1,1 @@
+hello ther
