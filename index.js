64ebf0aa72a1562c84889373b21aa7b6ce0f5b88
aivantg <aivant.developer@gmail.com>
2020-03-01 20:34:59 -0800

    switched to es6 import syntax, added airtable, began generation

--- index.js
+++ index.js
@@ -1,27 +1,71 @@
+import moment from 'moment';
+import {
+  createGeneration,
+  createPGEUsage,
+  getSolarProjectById,
+  getOwnerById
+} from './airtable/request';
+import getEnphaseData from './utils/enphase';
+
 require('dotenv-safe').config(); // Set up environment variables
-const fetch = require('node-fetch'); // Import fetch (for network requests)
-const moment = require('moment');
 
-const EnphaseUtils = require('./utils/enphase.js');
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-async function main() {
-  const result = await generateBill(
-    process.env.SAMPLE_ENPHASE_USER_ID,
-    process.env.SAMPLE_ENPHASE_SYSTEM_ID,
-    moment().subtract(30, 'days'),
-    moment()
-  );
-}
+// app.post('/generate', (req, res) => res.send('Hello World!'))
 
-async function generateBill(userId, systemId, startDate, endDate) {
-  const generationData = await EnphaseUtils.getEnphaseData(
-    userId,
-    systemId,
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const SampleUtilityData = {
+  netGeneration: 1000,
+  ebceRebate: 10,
+  startDate: moment().subtract(30, 'days'),
+  endDate: moment()
+};
+
+const generateBillForSubscriber = async (subscriber, solarProject) => {
+  const { netUsage, ebceRebate, startDate, endDate } = SampleUtilityData;
+  const generationData = await getEnphaseData(
+    subscriber.id,
+    solarProject.enphaseUserId,
+    solarProject.enphaseSystemId,
     startDate,
     endDate
   );
 
-  const utilityData = { netGeneration: 1000, ebceRebate: 10 };
+  const systemGeneration = generationData.reduce((a, b) => a + b, 0);
+
+  createGeneration({
+    amount: systemGeneration,
+    solarProjectId: [solarProject.id],
+    subscriberOwnerId: [subscriber.id],
+    startDate: startDate.format('MM/DD/YYYY'),
+    endDate: endDate.format('MM/DD/YYYY')
+  });
+  createPGEUsage({
+    solarProjectId: [solarProject.id],
+    subscriberOwnerId: [subscriber.id],
+    startDate: startDate.format('MM/DD/YYYY'),
+    endDate: endDate.format('MM/DD/YYYY'),
+    netUsage,
+    ebceRebate
+  });
+};
+
+const generateBillsForSolarProject = async solarProjectId => {
+  // const subscribers = getOwnersBySolarProjectId(solarProjectId);
+  const solarProject = await getSolarProjectById(solarProjectId);
+  const subscribers = await Promise.all(
+    solarProject.subscriberOwnerIds.map(id => getOwnerById(id))
+  );
+  subscribers.forEach(subscriber => {
+    generateBillForSubscriber(subscriber);
+  });
+};
+
+async function main() {
+  generateBillsForSolarProject('');
 }
 
 main();
--- utils/enphase.js
+++ utils/enphase.js
@@ -4,9 +4,26 @@ const moment = require('moment');
 const baseUrl = 'https://api.enphaseenergy.com/api/v2';
 const key = process.env.ENPHASE_KEY;
 
+const EnphaseSettings = {};
+
+// Takes the raw Enphase Response and gets the subscriber specific data out of it.
+// Returns array of production values
+const getDataForSubscriber = (subscriberId, data) => {
+  const process = EnphaseSettings[subscriberId];
+  if (process) {
+    return process(data);
+  }
+  return [];
+};
+
 // SEE ENPHASE API DOCS HERE https://developer.enphase.com/docs
-// Returns object with `production`, `meter_production`, `micro_production` keys with production data
-const getEnphaseData = async (userId, systemId, startDate, endDate) => {
+const getEnphaseData = async (
+  subscriberId,
+  userId,
+  systemId,
+  startDate,
+  endDate
+) => {
   // Build API Url
 
   const queryData = {
@@ -20,9 +37,8 @@ const getEnphaseData = async (userId, systemId, startDate, endDate) => {
   const queryString = new URLSearchParams(queryData).toString();
   const url = `${baseUrl}/systems/${systemId}/energy_lifetime?${queryString}`;
   const response = await fetch(url);
-  return await response.json();
+  const data = await response.json();
+  return getDataForSubscriber(subscriberId, data);
 };
 
-module.exports = {
-  getEnphaseData
-};
+export default getEnphaseData;
