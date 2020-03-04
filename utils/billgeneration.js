6e7f3bc54a7f6cd354b2f131515b5e9fa0cb6f7e
aivantg <aivant.developer@gmail.com>
2020-03-03 20:19:00 -0800

    added PG&E verification

--- utils/billgeneration.js
+++ utils/billgeneration.js
@@ -18,26 +18,15 @@ dotenv.config();
 const generateBillForSubscriber = async (subscriber, solarProject) => {
   // Get data necessary to generate bill
   console.log(`Generating Bill for ${subscriber.name}`);
-  const { netPgeUsage, ebceRebate, startDate, endDate } = await getLatestBill(
-    subscriber.meterId
-  );
+  const {
+    netPgeUsage,
+    ebceRebate,
+    startDate: startMoment,
+    endDate: endMoment
+  } = await getLatestBill(subscriber.meterId);
 
-  console.log(
-    `Found PGE Data for ${subscriber.name}
-Net Usage: ${netPgeUsage}
-ebce Rebate: ${ebceRebate}
-start date: ${startDate.format('MM/DD/YYYY')}
-end date: ${endDate.format('MM/DD/YYYY')}`
-  );
-  const generationData = await getEnphaseData(
-    subscriber.id,
-    solarProject.enphaseUserId,
-    solarProject.enphaseSystemId,
-    startDate,
-    endDate
-  );
-  console.log(`Found Enphase Data for ${subscriber.name}`);
-  console.log(generationData);
+  const startDate = startMoment.format('MM/DD/YYYY');
+  const endDate = endMoment.format('MM/DD/YYYY');
 
   let prevBill;
   if (subscriber.latestBillNumber !== 0) {
@@ -52,19 +41,48 @@ end date: ${endDate.format('MM/DD/YYYY')}`
     prevBill = {
       statementNumber: 0,
       totalEstimatedRebate: 0,
-      balance: 0
+      balance: 0,
+      startDate: '',
+      endDate: ''
     };
   }
+
+  if (
+    startDate === moment(prevBill.startDate).format('MM/DD/YYYY') ||
+    endDate === moment(prevBill.endDate).format('MM/DD/YYYY')
+  ) {
+    console.log('This PG&E bill has already been processed. Skipping!');
+    return;
+  }
+
+  console.log(
+    `Found PGE Data for ${subscriber.name}
+Net Usage: ${netPgeUsage}
+ebce Rebate: ${ebceRebate}
+start date: ${startDate}
+end date: ${endDate}`
+  );
+  const generationData = await getEnphaseData(
+    subscriber.id,
+    solarProject.enphaseUserId,
+    solarProject.enphaseSystemId,
+    startMoment,
+    endMoment
+  );
+  console.log(`Found Enphase Data for ${subscriber.name}`);
+  console.log(generationData);
 
   const rateSchedule = await getRateScheduleById(subscriber.rateScheduleId);
   const systemProduction = generationData.reduce((a, b) => a + b, 0);
   console.log(
-    `Generating Bill with Statement Number: ${prevBill.statementNumber + 1}`
+    `Saving Bill with Statement Number: ${prevBill.statementNumber + 1} for ${
+      subscriber.name
+    }`
   );
 
   createSubscriberBill({
-    startDate: startDate.format('MM/DD/YYYY'),
-    endDate: endDate.format('MM/DD/YYYY'),
+    startDate,
+    endDate,
     statementDate: moment().format('MM/DD/YYYY'),
     subscriberId: [subscriber.id],
     solarProjectId: [solarProject.id],
