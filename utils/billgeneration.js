513b81440d30526e8a99e4b10237309ddbe0b425
Aivant Goyal <aivant.developer@gmail.com>
2020-03-14 16:06:59 -0700

    PDF Generation (#4)
    * started pdf stuff
    
    * finished basic billing PDF cycle
    
    * rounded numbers on PDF
    
    * added comment
    
    * removed comments for Fang Shuo Deng
    
    * moved babel to real dependency
    
    * removed console log statement
    
    * fixed typo
    
    * corrected a unit error
    
    * more verbose logs
    
    * added await to PDF render
    
    * added await to PDF render
    
    * verbose logs
    
    * fixed nits and bugs
    
    * moved delay to constants

--- utils/billgeneration.js
+++ utils/billgeneration.js
@@ -1,19 +1,65 @@
 /* eslint-disable import/imports-first */
 import moment from 'moment';
 import dotenv from 'dotenv-safe';
+import fs from 'fs';
 
+import React from 'react';
+import ReactPDF from '@react-pdf/renderer';
 import {
   getSolarProjectById,
   getOwnerById,
   getRateScheduleById,
   createSubscriberBill,
   updateSubscriberBill,
-  getSubscriberBillsByStatementNumber
+  getSubscriberBillsByStatementNumber,
+  getSubscriberBillById
 } from '../airtable/request';
 import getEnphaseData from './enphase';
 import getLatestBill from './utilityApi';
+import BillingTemplate from './pdf/BillingTemplate';
+import Constants from '../Constants';
 
 dotenv.config();
+
+const generatePdf = async (
+  subscriber,
+  solarProject,
+  subscriberBillId,
+  prevBillId
+) => {
+  // Refresh information about existing bills to get the latest calculated values
+  const subscriberBill = await getSubscriberBillById(subscriberBillId);
+  let prevBill;
+  if (prevBillId) {
+    prevBill = await getSubscriberBillById(prevBillId);
+  } else {
+    prevBill = {
+      amountDue: 0,
+      amountReceived: 0,
+      balance: 0
+    };
+  }
+  console.log(`Creating PDF for Bill# ${subscriberBill.id} ...`);
+  await ReactPDF.render(
+    <BillingTemplate
+      subscriber={subscriber}
+      solarProject={solarProject}
+      subscriberBill={subscriberBill}
+      prevBill={prevBill}
+    />,
+    `./temp/${subscriberBill.id}.pdf`
+  );
+  await updateSubscriberBill(subscriberBill.id, {
+    billPdf: [{ url: `${Constants.ROOT_URL}/${subscriberBill.id}.pdf` }]
+  });
+  console.log(
+    `Succesfully uploaded PDF at ${Constants.ROOT_URL}/${subscriberBill.id}.pdf`
+  );
+  setTimeout(() => {
+    console.log(`Deleting Temporary PDF: ${subscriberBill.id}.pdf`);
+    fs.unlinkSync(`./temp/${subscriberBill.id}.pdf`);
+  }, Constants.PDF_DELETE_DELAY * 1000);
+};
 
 const generateBillForSubscriber = async (subscriber, solarProject) => {
   // Get data necessary to generate bill
@@ -74,17 +120,20 @@ end date: ${endDate}`
   console.log(generationData);
 
   const rateSchedule = await getRateScheduleById(subscriber.rateScheduleId);
-  const systemProduction = generationData.reduce((a, b) => a + b, 0);
+  const systemProduction = generationData.reduce((a, b) => a + b, 0) / 1000;
   console.log(
     `Saving Bill with Statement Number: ${prevBill.statementNumber + 1} for ${
       subscriber.name
     }`
   );
 
-  await createSubscriberBill({
+  const newBillId = await createSubscriberBill({
     startDate,
     endDate,
     statementDate: moment().format('MM/DD/YYYY'),
+    dueDate: moment()
+      .add(1, 'M')
+      .format('MM/DD/YYYY'),
     subscriberId: [subscriber.id],
     solarProjectId: [solarProject.id],
     rateScheduleId: [rateSchedule.id],
@@ -96,6 +145,9 @@ end date: ${endDate}`
     balanceOnPreviousBill: prevBill.balance,
     status: 'Pending'
   });
+
+  // Generate PDF!
+  generatePdf(subscriber, solarProject, newBillId, prevBill.id);
 };
 
 const generateBillsForSolarProject = async solarProjectId => {
