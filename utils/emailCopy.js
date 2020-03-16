084ddafabd573645961c8021538c9926f0304c4b
aivantg <aivant.developer@gmail.com>
2020-03-16 00:13:44 -0500

    moves admin email to environment variables

--- utils/emailCopy.js
+++ utils/emailCopy.js
@@ -1,5 +1,6 @@
-import Constants from '../Constants';
+require('dotenv-safe').config();
 
+const { ADMIN_EMAIL } = process.env;
 const HTML_LOGO = `<img src=""/>`;
 const PLAINTEXT_ADMIN_EMAIL_OPENER = `Dear Admin,\n\n`;
 const PLAINTEXT_ADMIN_EMAIL_CLOSER = `\n\nSincerely,\nYour Friendly PeoplePower Server`;
@@ -21,7 +22,7 @@ export default {
 
   // Generic Bill Error
   genericBillError: (subscriber, solarProject, errorMessage) => ({
-    to: Constants.ADMIN_EMAIL,
+    to: ADMIN_EMAIL,
     subject: 'Subscriber Bill Generation Failed',
     text: `${PLAINTEXT_ADMIN_EMAIL_OPENER}Bill Generation failed when generating bill for ${subscriber.name}. Their Subscriber ID in Airtable is ${subscriber.name}. They belong to the ${solarProject.name} project. Please check the server logs for issues and try again!\n\nError Message:${errorMessage}${PLAINTEXT_ADMIN_EMAIL_CLOSER}`,
 
@@ -31,7 +32,7 @@ export default {
 
   // PDF Bill Error
   pdfBillError: (subscriber, solarProject) => ({
-    to: Constants.ADMIN_EMAIL,
+    to: ADMIN_EMAIL,
     subject: 'Subscriber Bill PDF Generation Failed',
     text: `${PLAINTEXT_ADMIN_EMAIL_OPENER}While generating a bill for ${subscriber.name} (ID: ${subscriber.id}, Solar Project: ${solarProject.name}), the PDF generation process failed. The bill has been created successfully in Airtable but without PDF. Please delete the record created in error, check the server logs for issues and try again!${PLAINTEXT_ADMIN_EMAIL_CLOSER}`,
 
@@ -41,7 +42,7 @@ export default {
 
   // Stale PG&E Bill Found
   stalePGEBillError: (subscriber, solarProject) => ({
-    to: Constants.ADMIN_EMAIL,
+    to: ADMIN_EMAIL,
     subject: 'Subscriber Bill Generation Aborted',
     text: `${PLAINTEXT_ADMIN_EMAIL_OPENER}While generating a bill for ${subscriber.name} (ID: ${subscriber.id}, Solar Project: ${solarProject.name}), the generation process stopped because the subscriber has not had a new PG&E Billing Cycle since the last bill. This will not affect bill generation for other subscribers in the solar project.${PLAINTEXT_ADMIN_EMAIL_CLOSER}`,
 
@@ -51,7 +52,7 @@ export default {
 
   // Couldn't find Enphase Data
   missingEnphaseDataError: (subscriber, solarProject, startDate, endDate) => ({
-    to: Constants.ADMIN_EMAIL,
+    to: ADMIN_EMAIL,
     subject: 'Subscriber Bill Generation Aborted',
     text: `${PLAINTEXT_ADMIN_EMAIL_OPENER}While generating a bill for ${subscriber.name} (ID: ${subscriber.id}, Solar Project: ${solarProject.name}), the generation process stopped because it could not find any associated Enphase production data. It was looking for data over the time frame specified in the subscriber's latest PG&E Bill: ${startDate} - ${endDate}. This is likely because you have not updated the server code to include a processing function for this subscriber. This also might be due to an error in Enphase parameters (System ID and User ID), which are stored on Airtable under the Solar Project record.${PLAINTEXT_ADMIN_EMAIL_CLOSER}`,
 
@@ -61,7 +62,7 @@ export default {
 
   // Success Message on Bill Creation
   billSuccess: (subscriber, solarProject, subscriberBill, approveLink) => ({
-    to: Constants.ADMIN_EMAIL,
+    to: ADMIN_EMAIL,
     subject: 'Successfully Generated New Subscriber Bill',
     text: `${PLAINTEXT_ADMIN_EMAIL_OPENER}A bill has been successfully generated for Account #${
       subscriber.subscriberAccountNumber
